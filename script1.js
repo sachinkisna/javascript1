e8bbcbfb3968b872f2e415a3e79b85c96331f2d1
Sachinkisna <sachinsoftware0001@gmail.com>
2024-12-04 16:52:46 +0530

    data type in js

--- script1.js
+++ script1.js
@@ -1,4 +1,48 @@
-console.log("Hare Krishna Dear");
-console.log("Hare Krishna Dearwwww");
-console.log("Hare Krishna Dearyyyyyyyyyyyyyy");
-alert("Hare krishna");
+// console.log("Hare Krishna Dear");
+// console.log("Hare Krishna Dearwwww");
+// console.log("Hare Krishna Dearyyyyyyyyyyyyyy");
+// alert("Hare krishna");
+
+//variable----------->
+
+// let name="hare";
+// // ES-6 Echma script
+// let age=87;
+// // let age=65;
+// const nme="sgsgs";
+// // nme="shhs";
+// var fname="hhhs";
+// console.log(fname);
+// console.log(age);
+
+//<------------------Data Types----------->
+
+//Premitives
+let age=24;
+let fname="saschin";
+let isFollow=true;
+let x;
+let xn=null;
+let xy=BigInt('5423');
+let ys=Symbol('Hello');
+//non prememtive data types
+const student={
+    Fame:"Sacxin Kishna",
+    age:21,
+    isPass:true
+};
+student["age"]=student["age"]+1;
+console.log(student["age"])
+
+//practice object
+const product={
+    Name:"Body wash for everyday life",
+    ratings:4,
+    isDeal:true,
+    Offer:100,
+    Price:548
+
+};
+console.log(product);
+
+
